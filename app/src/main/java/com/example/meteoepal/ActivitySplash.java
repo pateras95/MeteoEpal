340c417f9186a8d16b56431553a0e7531184cd52
George <dddoumouras@hotmail.com>
2023-01-26 21:54:46 +0200

    Splash Screen Added

---
+++ app/src/main/java/com/example/meteoepal/ActivitySplash.java
@@ -0,0 +1,28 @@
+package com.example.meteoepal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(ActivitySplash.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+
+
+    }
+}
