0f5eb37a1b07c8b7f69f7bc145ca2fdbd0168b00
Konstantinos <kostasmc2@gmail.com>
2023-02-23 20:25:59 +0200

    add weather info for school inside nested webview and scroll view

--- app/src/main/java/com/example/meteoepal/ActivityAtalanti.java
+++ app/src/main/java/com/example/meteoepal/ActivityAtalanti.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import com.smarteist.autoimageslider.SliderView;
